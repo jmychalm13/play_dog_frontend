5e3565313633721f7413d1923249f787cd73e0cf
“Jessica <jmychalm13@gmail.com>
2024-01-14 06:21:40 -0600

    added usersIndex component and passing into content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,8 +1,10 @@
+import { UsersIndex } from "./UsersIndex";
+
 export function Content() {
   return (
     <div>
-      <h1>Welcome to PlayDog</h1>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <UsersIndex />
     </div>
   );
 }
---
+++ src/UsersIndex.jsx
@@ -0,0 +1,7 @@
+export function UsersIndex() {
+  return (
+    <div>
+      <h1>This is wherre all users will be displayed at the root page</h1>
+    </div>
+  );
+}
