b2d8793f27eda60ae083a10e9a007ca4900cff63
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-01-23 16:18:13 -0600

    worked on userShow page

--- src/UserShow.jsx
+++ src/UserShow.jsx
@@ -8,7 +8,7 @@ export function UserShow() {
 
   const handleGetUser = () => {
     axios.get(`http://localhost:3000/users/${params.id}.json`).then((response) => {
-      console.log(response.data);
+      console.log(response.data.dogs);
       setUserInfo(response.data);
     });
   };
@@ -19,8 +19,42 @@ export function UserShow() {
   return (
     <section className="bg-gradient-to-b from-gray-300 to-green-800 bg-cover bg-center h-screen">
       <h1 className="text-3xl text-center">Profile</h1>
-      <h3 className="text-xl">{userInfo.name}</h3>
-      <img src={userInfo.image_url} alt="" />
+      <div className="flex justify-center">
+        <div className="shadow-lg rounded-lg overflow-hidden w-1/4">
+          <h3 className="text-xl">
+            Username: <span className="text-large">{userInfo.name}</span>
+          </h3>
+          <img className="w-full" src={userInfo.image_url} alt="" />
+        </div>
+      </div>
+      <h2 className="text-center text-2xl underline">Dogs</h2>
+      <div className="dog-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {userInfo &&
+          userInfo.dogs &&
+          userInfo.dogs.map((dog) => (
+            <div className="flex justify-center" key={dog.id}>
+              <div className="bg-white shadow-lg rounded-lg overflow-hidden w-1/4">
+                <img className="w-full object-cover object-center" src={dog.image_url} alt="" />
+                <div className="p-4 flex justify-between items-center">
+                  <h2 className="text-gray-800 text-lg font-semibold">{dog.name}</h2>
+                  <h2 className="text-gray-800 text-lg font-semibold">{dog.age}</h2>
+                </div>
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-r from-emerald-400 via-emerald-500 to-emerald-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-emerald-300 dark:focus:ring-emerald-800 shadow-lg shadow-emerald-500/50 dark:shadow-lg dark:shadow-emerald-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                >
+                  More Info
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+      <button
+        type="button"
+        className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Edit
+      </button>
     </section>
   );
 }
