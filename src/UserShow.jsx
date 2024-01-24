c96723a046ceca4a6e8e94c601b54619638e9a11
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-01-24 07:13:49 -0600

    userShow almost complete, about to add testing

--- src/UserShow.jsx
+++ src/UserShow.jsx
@@ -27,7 +27,9 @@ export function UserShow() {
           <img className="w-full" src={userInfo.image_url} alt="" />
         </div>
       </div>
-      <h2 className="text-center text-2xl underline">Dogs</h2>
+      <div className="my-12">
+        <h2 className="text-center text-2xl underline">Dogs</h2>
+      </div>
       <div className="dog-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {userInfo &&
           userInfo.dogs &&
@@ -49,12 +51,14 @@ export function UserShow() {
             </div>
           ))}
       </div>
-      <button
-        type="button"
-        className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-      >
-        Edit
-      </button>
+      <div className="w-full flex justify-center">
+        <button
+          type="button"
+          className="my-12 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Edit Profile
+        </button>
+      </div>
     </section>
   );
 }
