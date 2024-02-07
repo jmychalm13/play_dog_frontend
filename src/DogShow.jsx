578fa1c661f7842bcbc80aeef0d048ea50b22bc7
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-02-07 10:03:58 -0600

    made some morestyling changes

--- src/DogShow.jsx
+++ src/DogShow.jsx
@@ -24,31 +24,28 @@ export function DogShow() {
   useEffect(getDogInfo, []);
 
   return (
-    <section className="h-screen w-screen bg-gradient-to-b from-violet-300 via-purple-300 to-fuchsia-300">
-      <p className="text-fuchsia-900 name py-3 text-4xl text-center">{dogInfo.name}</p>
-      <div className="flex justify-center">
+    <section className="grid place-items-center h-screen w-screen bg-gradient-to-b from-violet-300 via-purple-300 to-fuchsia-300">
+      <p className="text-fuchsia-900 name text-4xl text-center">{dogInfo.name}</p>
+      <div className="border-fuchsia-900 w-2/3 flex shadow-lg justify-center p-4">
         <div className="w-1/3">
           <img className="h-auto max-w-full rounded-lg" src={dogInfo.image_url} alt="image description" />
-          <figcaption className="mt-2 text-md text-center text-gray-500 dark:text-gray-400">
-            {dogInfo.age} years old
-          </figcaption>
-          <figcaption className="mt-2 text-md text-center text-gray-500 dark:text-gray-400">{dogInfo.breed}</figcaption>
+          <div className="info">
+            <p>
+              {dogInfo.name} is a {dogInfo.age} year old {dogInfo.breed}.
+            </p>
+            <p>These are some of {dogInfo.name}&apos;s most common characteristics:</p>
+            <div>
+              <ul className="list-disc list-inside">
+                {dogInfo &&
+                  dogInfo.behaviors &&
+                  dogInfo.behaviors.map((behavior) => <li key={behavior.id}>{behavior.behavior}</li>)}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="info">
-        <p>
-          {dogInfo.name} is a {dogInfo.age} year old {dogInfo.breed}.
-        </p>
-        <p>These are some of {dogInfo.name}&apos;s most common characteristics:</p>
-        <div>
-          <ul className="list-disc list-inside">
-            {dogInfo &&
-              dogInfo.behaviors &&
-              dogInfo.behaviors.map((behavior) => <li key={behavior.id}>{behavior.behavior}</li>)}
-          </ul>
-        </div>
-      </div>
-      <div className="border buttons">
+
+      <div className="bg-fuchsia-900 text-white p-2 rounded-lg shadow-lg border-fuchsia-900 buttons">
         {isLoggedIn ? <button>Edit Pup</button> : <button>Schedule Playdate</button>}
       </div>
     </section>
