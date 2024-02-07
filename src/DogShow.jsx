338d29d6a5d4a5c559fcc2a63067dc8f34bdbfd3
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-02-07 09:51:22 -0600

    fixed sizing issue with pic on DogShow

--- src/DogShow.jsx
+++ src/DogShow.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./DogShow.css";
 
 export function DogShow() {
   const [dogInfo, setDogInfo] = useState({});
@@ -12,6 +13,7 @@ export function DogShow() {
     const userId = +localStorage.getItem("userId");
     axios.get(`http://localhost:3000/dogs/${id}.json`).then((response) => {
       setDogInfo(response.data);
+      console.log("dogInfo:", response.data);
       if (response.data.user_id === userId) {
         setIsLoggedIn(true);
       }
@@ -23,13 +25,15 @@ export function DogShow() {
 
   return (
     <section className="h-screen w-screen bg-gradient-to-b from-violet-300 via-purple-300 to-fuchsia-300">
-      <p className="py-3 text-4xl text-center">{dogInfo.name}</p>
-      <div className="grid place-items-center">
-        <img className="h-auto max-w-full rounded-lg" src={dogInfo.image_url} alt="image description" />
-        <figcaption className="mt-2 text-md text-center text-gray-500 dark:text-gray-400">
-          {dogInfo.age} years old
-        </figcaption>
-        <figcaption className="mt-2 text-md text-center text-gray-500 dark:text-gray-400">{dogInfo.breed}</figcaption>
+      <p className="text-fuchsia-900 name py-3 text-4xl text-center">{dogInfo.name}</p>
+      <div className="flex justify-center">
+        <div className="w-1/3">
+          <img className="h-auto max-w-full rounded-lg" src={dogInfo.image_url} alt="image description" />
+          <figcaption className="mt-2 text-md text-center text-gray-500 dark:text-gray-400">
+            {dogInfo.age} years old
+          </figcaption>
+          <figcaption className="mt-2 text-md text-center text-gray-500 dark:text-gray-400">{dogInfo.breed}</figcaption>
+        </div>
       </div>
       <div className="info">
         <p>
@@ -44,7 +48,9 @@ export function DogShow() {
           </ul>
         </div>
       </div>
-      <div className="buttons">{isLoggedIn ? <button>Edit Pup</button> : <button>Schedule Playdate</button>}</div>
+      <div className="border buttons">
+        {isLoggedIn ? <button>Edit Pup</button> : <button>Schedule Playdate</button>}
+      </div>
     </section>
   );
 }
