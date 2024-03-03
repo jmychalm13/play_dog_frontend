b8f23faeeb67f9fb569d122ac1633f53d7c8b669
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-03-03 07:38:34 -0600

    added addDog component and began styling

--- src/DogNew.jsx
+++ src/DogNew.jsx
@@ -2,15 +2,18 @@ import { useHref } from "react-router-dom";
 
 export function DogNew() {
   return (
-    <div
-      className="relative min-h-screen flex items-center justify-center bg-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover"
-      style={{
-        backgroundImage: useHref(
-          "https://images.unsplash.com/photo-1518640165980-d3e0e2aa6c1e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        ),
-      }}
-    >
-      <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
+    <div className="relative h-screen">
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage: `url(https://images.unsplash.com/photo-1518640165980-d3e0e2aa6c1e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
+        }}
+      ></div>
+      <div className="absolute inset-0 bg-gray-900/40"></div>
+      <div className="z-10 relative w-1/2 flex items-center justify-center text-white ml-auto py-5">
+        <h1 className="text-3xl font-bold mb-4">Add Pet</h1>
+        <form action=""></form>
+      </div>
     </div>
   );
 }
