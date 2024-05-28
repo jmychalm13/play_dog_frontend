640527c2c2ff522e73d90d6bc0fee056e57342c0
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-05-28 16:12:02 -0500

    behavior rating working in add pet

--- src/DogNew.jsx
+++ src/DogNew.jsx
@@ -13,7 +13,6 @@ export function DogNew() {
     breed: "",
     name: "",
     age: 0,
-    behavior_attributes: behaviors,
   });
 
   const handleRatingChange = (behaviorName, rating) => {
@@ -48,10 +47,6 @@ export function DogNew() {
       formData.append("breed", dogData.breed);
       formData.append("age", dogData.age);
       formData.append("image_url", uploadedImg);
-      behaviors.forEach((behavior, index) => {
-        formData.append(`dog[behavior_attributes][${index}][behavior]`, behavior.behavior);
-        formData.append(`dog[behavior_attributes][${index}][rating]`, behavior.rating);
-      });
       axios
         .post("http://localhost:3000/dogs.json", formData, {
           headers: {
@@ -59,10 +54,17 @@ export function DogNew() {
           },
         })
         .then((response) => {
-          responseId = response.data.id;
-          const userId = localStorage.getItem("userId");
-          event.target.reset();
-          window.location.href = `/users/${userId}`;
+          const dogId = response.data.id;
+          console.log("response", response);
+          const behaviorData = {
+            dog_id: dogId,
+            behavior_attributes: behaviors,
+          };
+          axios.post("http://localhost:3000/behaviors.json", behaviorData).then((response) => {
+            console.log(response);
+          });
+          // event.target.reset();
+          // window.location.href = `/users/${userId}`;
         })
         .catch((error) => {
           console.log(error);
