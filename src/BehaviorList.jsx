0f18172679fe4a462fe6a0e8019c388de53adfc2
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-06-09 11:59:38 -0500

    removed behavior component to further condense

--- src/BehaviorList.jsx
+++ src/BehaviorList.jsx
@@ -1,11 +1,25 @@
 /* eslint-disable react/prop-types */
-import { Behavior } from "./Behavior";
 
 export function BehaviorList({ behaviors }) {
+  const stars = [];
+  const behaviors_array = [];
+  behaviors.map((behavior) => {
+    for (let i = 0; i < behavior.rating; i++) {
+      stars.push(<span style={{ color: "gold" }}>&#9733;</span>);
+    }
+  });
+
+  behaviors.map((behavior) => {
+    behaviors_array.push(behavior.behavior);
+  });
+
   return (
     <div>
-      {behaviors.map((behavior, index) => (
-        <Behavior key={index} behavior={behavior} />
+      {behaviors_array.map((behavior, index) => (
+        <div key={index}>
+          <p>{behavior}</p>
+          <div className="stars"></div>
+        </div>
       ))}
     </div>
   );
